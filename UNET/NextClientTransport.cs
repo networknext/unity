ff75f6585721b1289056076912d30686a8388d6a
nbopardi <nikhilb@gmail.com>
2022-04-05 12:30:02 -0700

    extend INetworkTransport from UNET for client and server

---
+++ UNET/NextClientTransport.cs
@@ -0,0 +1,314 @@
+using System;
+using System.Net;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Types;
+using NetworkNext;
+
+public class NextClientTransport : INetworkTransport
+{
+    public IntPtr client;
+    public IntPtr clientCtxPtr;
+    public Next.NextConfig nextConfig;
+    public string serverAddress;
+    public string clientBindAddress;
+    public NextClientPacketReceivedCallback clientPacketReceivedCallback;
+    public NextWakeupCallback clientWakeupCallback;
+
+    public NextClientTransport(IntPtr ctxPtr, ref Next.NextConfig config, string bindAddr, string serverAddr, NextClientPacketReceivedCallback packetRecvCallback, NextWakeupCallback wakeupCallback = null)
+    {
+        clientCtxPtr = ctxPtr;
+        nextConfig = config;
+        serverAddress = serverAddr;
+        clientBindAddress = bindAddr;
+        clientPacketReceivedCallback = packetRecvCallback;
+        clientWakeupCallback = wakeupCallback;
+
+        if (!IsClientReady())
+        {
+            if (Next.NextInit(clientCtxPtr, ref nextConfig) != Next.NEXT_OK)
+            {
+                throw new InvalidOperationException("could not initialize Network Next");
+            }
+
+            client = Next.NextClientCreate(clientCtxPtr, clientBindAddress, clientPacketReceivedCallback, clientWakeupCallback);
+            if (!IsClientReady())
+            {
+                throw new InvalidOperationException("could not create client");
+            }
+        }
+    }
+
+    #region Utility Functions
+
+    public bool IsClientReady()
+    {
+        return client != null && client != IntPtr.Zero;
+    }
+
+    public bool IsClientConnected()
+    {
+        return IsClientReady() && Next.NextClientState(client) == Next.NEXT_CLIENT_STATE_OPEN;
+    }
+
+    #endregion // #region Utility Functions
+
+    #region Next Client Functions
+
+    public void NextClientDestroy()
+    {
+        if (IsClientReady())
+        {
+            Next.NextClientDestroy(client);
+            client = IntPtr.Zero;
+        }
+    }
+
+    public ushort NextClientPort()
+    {
+        if (IsClientReady())
+        {
+            return Next.NextClientPort(client);
+        }
+        return 0;
+    }
+
+    public void NextClientOpenSession()
+    {
+        if (IsClientReady())
+        {
+            Next.NextClientOpenSession(client, serverAddress);
+        }
+    }
+
+    public void NextClientCloseSession()
+    {
+        if (IsClientConnected())
+        {
+            Next.NextClientCloseSession(client);
+        }
+    }
+
+    public bool NextClientIsSessionOpen()
+    {
+        return IsClientReady() && Next.NextClientIsSessionOpen(client);
+    }
+
+    public int NextClientState()
+    {
+        if (IsClientReady())
+        {
+            return Next.NextClientState(client);
+        }
+        return Next.NEXT_ERROR;
+    }
+
+    public void NextClientUpdate()
+    {
+        if (IsClientConnected())
+        {
+            Next.NextClientUpdate(client);
+        }
+    }
+
+    public void NextClientSendPacket(byte[] buffer, int size)
+    {
+        if (IsClientConnected())
+        {
+            Next.NextClientSendPacket(client, buffer, size);
+        }
+    }
+
+    public void NextClientSendPacketDirect(byte[] buffer, int size)
+    {
+        if (IsClientConnected())
+        {
+            Next.NextClientSendPacketDirect(client, buffer, size);
+        }
+    }
+
+    public void NextClientReportSession()
+    {
+        if (IsClientConnected())
+        {
+            Next.NextClientReportSession(client);
+        }
+    }
+
+    public ulong NextClientSessionID()
+    {
+        if (IsClientConnected())
+        {
+            return Next.NextClientSessionID(client);
+        }
+        return 0;
+    }
+
+    public Next.ClientStats NextClientStats()
+    {
+        if (IsClientConnected())
+        {
+            return Next.NextClientStats(client);
+        }
+        return new Next.ClientStats();
+    }
+
+    public Next.NextAddress NextClientServerAddress()
+    {
+        if (IsClientConnected())
+        {
+            return Next.NextClientServerAddress(client);
+        }
+        return new Next.NextAddress();
+    }
+
+    #endregion // #region Next Client Functions
+
+    #region INetworkTransport Functions
+
+    public bool IsStarted { get; }
+
+    public int AddHost(HostTopology topology, int port, string ip)
+    {
+        return NetworkTransport.AddHost(topology, port, ip);
+    }
+
+    public int AddHostWithSimulator(HostTopology topology, int minTimeout, int maxTimeout, int port)
+    {
+        return NetworkTransport.AddHostWithSimulator(topology, minTimeout, maxTimeout, port);
+    }
+
+    public int AddWebsocketHost(HostTopology topology, int port, string ip)
+    {
+        return NetworkTransport.AddWebsocketHost(topology, port, ip);
+    }
+
+    public int Connect(int hostId, string address, int port, int specialConnectionId, out byte error)
+    {
+        return NetworkTransport.Connect(hostId, address, port, specialConnectionId, out error);
+    }
+
+    public void ConnectAsNetworkHost(int hostId, string address, int port, NetworkID network, SourceID source, NodeID node, out byte error)
+    {
+        NetworkTransport.ConnectAsNetworkHost(hostId, address, port, network, source, node, out error);
+    }
+
+    public int ConnectEndPoint(int hostId, EndPoint endPoint, int specialConnectionId, out byte error)
+    {
+        return NetworkTransport.ConnectEndPoint(hostId, endPoint, specialConnectionId, out error);
+    }
+
+    public int ConnectToNetworkPeer(int hostId, string address, int port, int specialConnectionId, int relaySlotId, NetworkID network, SourceID source, NodeID node, out byte error)
+    {
+        return NetworkTransport.ConnectToNetworkPeer(hostId, address, port, specialConnectionId, relaySlotId, network, source, node, out error);
+    }
+
+    public int ConnectWithSimulator(int hostId, string address, int port, int specialConnectionId, out byte error, ConnectionSimulatorConfig conf)
+    {
+        return NetworkTransport.ConnectWithSimulator(hostId, address, port, specialConnectionId, out error, conf);
+    }
+
+    public bool Disconnect(int hostId, int connectionId, out byte error)
+    {
+        NextClientCloseSession();
+        return NetworkTransport.Disconnect(hostId, connectionId, out error);
+    }
+
+    public bool DoesEndPointUsePlatformProtocols(EndPoint endPoint)
+    {
+        throw new NotImplementedException("NextClientTransport does not support DoesEndPointUsePlatformProtocols");
+    }
+
+    public void GetBroadcastConnectionInfo(int hostId, out string address, out int port, out byte error)
+    {
+        NetworkTransport.GetBroadcastConnectionInfo(hostId, out address, out port, out error);
+    }
+
+    public void GetBroadcastConnectionMessage(int hostId, byte[] buffer, int bufferSize, out int receivedSize, out byte error)
+    {
+        NetworkTransport.GetBroadcastConnectionMessage(hostId, buffer, bufferSize, out receivedSize, out error);
+    }
+
+    public void GetConnectionInfo(int hostId, int connectionId, out string address, out int port, out NetworkID network, out NodeID dstNode, out byte error)
+    {
+        NetworkTransport.GetConnectionInfo(hostId, connectionId, out address, out port, out network, out dstNode, out error);
+    }
+
+    public int GetCurrentRTT(int hostId, int connectionId, out byte error)
+    {
+        if (IsClientConnected())
+        {
+            error = (byte)NetworkError.Ok;
+            Next.ClientStats clientStats = NextClientStats();
+            if (clientStats.Next)
+            {
+                return (int)clientStats.NextRTT;
+            }
+            return (int)clientStats.DirectMinRTT;
+        }
+        return NetworkTransport.GetCurrentRTT(hostId, connectionId, out error);
+    }
+
+    public void Init()
+    {
+        NetworkTransport.Init();
+    }
+
+    public void Init(GlobalConfig config)
+    {
+        NetworkTransport.Init(config);
+    }
+
+    public NetworkEventType Receive(out int hostId, out int connectionId, out int channelId, byte[] buffer, int bufferSize, out int receivedSize, out byte error)
+    {
+        return NetworkTransport.Receive(out hostId, out connectionId, out channelId, buffer, bufferSize, out receivedSize, out error);
+    }
+
+    public NetworkEventType ReceiveFromHost(int hostId, out int connectionId, out int channelId, byte[] buffer, int bufferSize, out int receivedSize, out byte error)
+    {
+        return NetworkTransport.ReceiveFromHost(hostId, out connectionId, out channelId, buffer, bufferSize, out receivedSize, out error);
+    }
+
+    public NetworkEventType ReceiveRelayEventFromHost(int hostId, out byte error)
+    {
+        return NetworkTransport.ReceiveRelayEventFromHost(hostId, out error);
+    }
+
+    public bool RemoveHost(int hostId)
+    {
+        return NetworkTransport.RemoveHost(hostId);
+    }
+
+    public bool Send(int hostId, int connectionId, int channelId, byte[] buffer, int size, out byte error)
+    {
+        return NetworkTransport.Send(hostId, connectionId, channelId, buffer, size, out error);
+    }
+
+    public void SetBroadcastCredentials(int hostId, int key, int version, int subversion, out byte error)
+    {
+        NetworkTransport.SetBroadcastCredentials(hostId, key, version, subversion, out error);
+    }
+
+    public void SetPacketStat(int direction, int packetStatId, int numMsgs, int numBytes)
+    {
+        NetworkTransport.SetPacketStat(direction, packetStatId, numMsgs, numBytes);
+    }
+
+    public void Shutdown()
+    {
+        NetworkTransport.Shutdown();
+        NextClientDestroy();
+        Next.NextTerm();
+    }
+
+    public bool StartBroadcastDiscovery(int hostId, int broadcastPort, int key, int version, int subversion, byte[] buffer, int size, int timeout, out byte error)
+    {
+        return NetworkTransport.StartBroadcastDiscovery(hostId, broadcastPort, key, version, subversion, buffer, size, timeout, out error);
+    }
+
+    public void StopBroadcastDiscovery()
+    {
+        NetworkTransport.StopBroadcastDiscovery();
+    }
+
+    #endregion // #region INetworkTransport Functions
+}
---
+++ UNET/NextServerTransport.cs
@@ -0,0 +1,331 @@
+using System;
+using System.Net;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Types;
+using NetworkNext;
+
+public class NextServerTransport : INetworkTransport
+{
+    public IntPtr server;
+    public IntPtr serverCtxPtr;
+    public Next.NextConfig nextConfig;
+    public string serverAddress;
+    public string serverBindAddress;
+    public string serverDatacenter;
+    public NextServerPacketReceivedCallback serverPacketReceivedCallback;
+    public NextWakeupCallback serverWakeupCallback;
+
+    public NextServerTransport(IntPtr ctxPtr, ref Next.NextConfig config, string serverAddr, string bindAddr, string datacenter, NextServerPacketReceivedCallback packetRecvCallback, NextWakeupCallback wakeupCallback = null)
+    {
+        serverCtxPtr = ctxPtr;
+        nextConfig = config;
+        serverAddress = serverAddr;
+        serverBindAddress = bindAddr;
+        serverDatacenter = datacenter;
+        serverPacketReceivedCallback = packetRecvCallback;
+        serverWakeupCallback = wakeupCallback;
+
+        if (!IsServerReady())
+        {
+            if (Next.NextInit(serverCtxPtr, ref nextConfig) != Next.NEXT_OK)
+            {
+                throw new InvalidOperationException("could not initialize Network Next");
+            }
+
+            server = Next.NextServerCreate(serverCtxPtr, serverAddress, serverBindAddress, serverDatacenter, serverPacketReceivedCallback, serverWakeupCallback);
+            if (server == null || server == IntPtr.Zero)
+            {
+                throw new InvalidOperationException("could not create server");
+            }
+        }
+    }
+
+    #region Utility Functions
+
+    public bool IsServerReady()
+    {
+        return server != null && server != IntPtr.Zero;
+    }
+
+    #endregion // #region Utility Functions
+
+    #region Next Server Functions
+
+    public void NextServerDestroy()
+    {
+        if (IsServerReady())
+        {
+            Next.NextServerDestroy(server);
+            server = IntPtr.Zero;
+        }
+    }
+
+    public ushort NextServerPort()
+    {
+        if (IsServerReady())
+        {
+            return Next.NextServerPort(server);
+        }
+        return 0;
+    }
+
+    public Next.NextAddress NextServerAddress()
+    {
+        if (IsServerReady())
+        {
+            return Next.NextServerAddress(server);
+        }
+        return new Next.NextAddress();
+    }
+
+    public int NextServerState()
+    {
+        if (IsServerReady())
+        {
+            return Next.NextServerState(server);
+        }
+        return Next.NEXT_ERROR;
+    }
+
+    public void NextServerUpdate()
+    {
+        if (IsServerReady())
+        {
+            Next.NextServerUpdate(server);
+        }
+    }
+
+    public ulong NextServerUpgradeSession(IntPtr addressPtr, string userID)
+    {
+        if (IsServerReady())
+        {
+            return Next.NextServerUpgradeSession(server, addressPtr, userID);
+        }
+        return 0;
+    }
+
+    public void NextServerTagSession(IntPtr addressPtr, string tag)
+    {
+        if (IsServerReady())
+        {
+            Next.NextServerTagSession(server, addressPtr, tag);
+        }
+    }
+
+    public void NextServerTagSessionMultiple(IntPtr addressPtr, string[] tags, int numTags)
+    {
+        if (IsServerReady())
+        {
+            Next.NextServerTagSessionMultiple(server, addressPtr, tags, numTags);
+        }
+    }
+
+    public bool NextServerSessionUpgraded(IntPtr addressPtr)
+    {
+        return IsServerReady() && Next.NextServerSessionUpgraded(server, addressPtr);
+    }
+
+    public void NextServerSendPacket(IntPtr clientAddr, byte[] buffer, int size)
+    {
+        if (IsServerReady())
+        {
+            Next.NextServerSendPacket(server, clientAddr, buffer, size);
+        }
+    }
+
+    public void NextServerSendPacketDirect(IntPtr clientAddr, byte[] buffer, int size)
+    {
+        if (IsServerReady())
+        {
+            Next.NextServerSendPacketDirect(server, clientAddr, buffer, size);
+        }
+    }
+
+    public bool NextServerStats(IntPtr addressPtr, out Next.ServerStats stats)
+    {
+        if (IsServerReady())
+        {
+            return Next.NextServerStats(server, addressPtr, out stats);
+        }
+        stats = new Next.ServerStats();
+        return false;
+    }
+
+    public bool NextServerAutodetectFinished()
+    {
+        return IsServerReady() && Next.NextServerAutodetectFinished(server);
+    }
+
+    public string NextServerAutodetectedDatacenter()
+    {
+        if (IsServerReady())
+        {
+            return Next.NextServerAutodetectedDatacenter(server);
+        }
+        return serverDatacenter;
+    }
+
+    public void NextServerEvent(IntPtr addressPtr, ulong serverEvents)
+    {
+        if (IsServerReady())
+        {
+            Next.NextServerEvent(server, addressPtr, serverEvents);
+        }
+    }
+
+    public void NextServerMatch(IntPtr addressPtr, string matchID, double[] matchValues, int numMatchValues)
+    {
+        if (IsServerReady())
+        {
+            Next.NextServerMatch(server, addressPtr, matchID, matchValues, numMatchValues);
+        }
+    }
+
+    public void NextServerFlush()
+    {
+        if (IsServerReady())
+        {
+            Next.NextServerFlush(server);
+        }
+    }
+
+    #endregion // #region Next Server Functions
+
+    #region INetworkTransport Functions
+
+    public bool IsStarted { get; }
+
+    public int AddHost(HostTopology topology, int port, string ip)
+    {
+        return NetworkTransport.AddHost(topology, port, ip);
+    }
+
+    public int AddHostWithSimulator(HostTopology topology, int minTimeout, int maxTimeout, int port)
+    {
+        return NetworkTransport.AddHostWithSimulator(topology, minTimeout, maxTimeout, port);
+    }
+
+    public int AddWebsocketHost(HostTopology topology, int port, string ip)
+    {
+        return NetworkTransport.AddWebsocketHost(topology, port, ip);
+    }
+
+    public int Connect(int hostId, string address, int port, int specialConnectionId, out byte error)
+    {
+        return NetworkTransport.Connect(hostId, address, port, specialConnectionId, out error);
+    }
+
+    public void ConnectAsNetworkHost(int hostId, string address, int port, NetworkID network, SourceID source, NodeID node, out byte error)
+    {
+        NetworkTransport.ConnectAsNetworkHost(hostId, address, port, network, source, node, out error);
+    }
+
+    public int ConnectEndPoint(int hostId, EndPoint endPoint, int specialConnectionId, out byte error)
+    {
+        return NetworkTransport.ConnectEndPoint(hostId, endPoint, specialConnectionId, out error);
+    }
+
+    public int ConnectToNetworkPeer(int hostId, string address, int port, int specialConnectionId, int relaySlotId, NetworkID network, SourceID source, NodeID node, out byte error)
+    {
+        return NetworkTransport.ConnectToNetworkPeer(hostId, address, port, specialConnectionId, relaySlotId, network, source, node, out error);
+    }
+
+    public int ConnectWithSimulator(int hostId, string address, int port, int specialConnectionId, out byte error, ConnectionSimulatorConfig conf)
+    {
+        return NetworkTransport.ConnectWithSimulator(hostId, address, port, specialConnectionId, out error, conf);
+    }
+
+    public bool Disconnect(int hostId, int connectionId, out byte error)
+    {
+        return NetworkTransport.Disconnect(hostId, connectionId, out error);
+    }
+
+    public bool DoesEndPointUsePlatformProtocols(EndPoint endPoint)
+    {
+        throw new NotImplementedException("NextServerTransport does not support DoesEndPointUsePlatformProtocols");
+    }
+
+    public void GetBroadcastConnectionInfo(int hostId, out string address, out int port, out byte error)
+    {
+        NetworkTransport.GetBroadcastConnectionInfo(hostId, out address, out port, out error);
+    }
+
+    public void GetBroadcastConnectionMessage(int hostId, byte[] buffer, int bufferSize, out int receivedSize, out byte error)
+    {
+        NetworkTransport.GetBroadcastConnectionMessage(hostId, buffer, bufferSize, out receivedSize, out error);
+    }
+
+    public void GetConnectionInfo(int hostId, int connectionId, out string address, out int port, out NetworkID network, out NodeID dstNode, out byte error)
+    {
+        NetworkTransport.GetConnectionInfo(hostId, connectionId, out address, out port, out network, out dstNode, out error);
+    }
+
+    public int GetCurrentRTT(int hostId, int connectionId, out byte error)
+    {
+        return NetworkTransport.GetCurrentRTT(hostId, connectionId, out error);
+    }
+
+    public void Init() {
+
+        NetworkTransport.Init();
+    }
+
+    public void Init(GlobalConfig config)
+    {
+        NetworkTransport.Init(config);
+    }
+
+    public NetworkEventType Receive(out int hostId, out int connectionId, out int channelId, byte[] buffer, int bufferSize, out int receivedSize, out byte error)
+    {
+        return NetworkTransport.Receive(out hostId, out connectionId, out channelId, buffer, bufferSize, out receivedSize, out error);
+    }
+
+    public NetworkEventType ReceiveFromHost(int hostId, out int connectionId, out int channelId, byte[] buffer, int bufferSize, out int receivedSize, out byte error)
+    {
+        return NetworkTransport.ReceiveFromHost(hostId, out connectionId, out channelId, buffer, bufferSize, out receivedSize, out error);
+    }
+
+    public NetworkEventType ReceiveRelayEventFromHost(int hostId, out byte error)
+    {
+        return NetworkTransport.ReceiveRelayEventFromHost(hostId, out error);
+    }
+
+    public bool RemoveHost(int hostId)
+    {
+        return NetworkTransport.RemoveHost(hostId);
+    }
+
+    public bool Send(int hostId, int connectionId, int channelId, byte[] buffer, int size, out byte error)
+    {
+        return NetworkTransport.Send(hostId, connectionId, channelId, buffer, size, out error);
+    }
+
+    public void SetBroadcastCredentials(int hostId, int key, int version, int subversion, out byte error)
+    {
+        NetworkTransport.SetBroadcastCredentials(hostId, key, version, subversion, out error);
+    }
+
+    public void SetPacketStat(int direction, int packetStatId, int numMsgs, int numBytes)
+    {
+        NetworkTransport.SetPacketStat(direction, packetStatId, numMsgs, numBytes);
+    }
+
+    public void Shutdown()
+    {
+        NetworkTransport.Shutdown();
+        NextServerDestroy();
+        Next.NextTerm();
+    }
+
+    public bool StartBroadcastDiscovery(int hostId, int broadcastPort, int key, int version, int subversion, byte[] buffer, int size, int timeout, out byte error)
+    {
+        return NetworkTransport.StartBroadcastDiscovery(hostId, broadcastPort, key, version, subversion, buffer, size, timeout, out error);
+    }
+
+    public void StopBroadcastDiscovery()
+    {
+        NetworkTransport.StopBroadcastDiscovery();
+    }
+
+    #endregion // #region INetworkTransport Functions
+}
